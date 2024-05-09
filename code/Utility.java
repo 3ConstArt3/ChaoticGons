04147eb0f2f3b21e520ab9d7a094db93578e37c0
ConstArt <114258174+3ConstArt3@users.noreply.github.com>
2024-05-09 17:16:15 +0300

    Update Utility.java

--- code/Utility.java
+++ code/Utility.java
@@ -13,11 +13,24 @@ class Utility
     var distYSquared = pow(pointA.y - pointB.y, 2);
     return sqrt(distXSquared + distYSquared);
   }
-  
+
   public PVector lerpedPoint(PVector pointA, PVector pointB, float percentage)
   {
     var lerpPosX = lerp(pointA.x, pointB.x, percentage);
     var lerpPosY = lerp(pointA.y, pointB.y, percentage);
     return new PVector(lerpPosX, lerpPosY);
+  }
+
+  public boolean differBy(int[] indices, int[] params)
+  {
+    var currentIndex = indices[0];
+    var previousIndex = indices[1];
+    var distance = params[0];
+    var limit = params[1];
+
+    var neighborA = (currentIndex + distance + limit) % limit;
+    var neighborB = (currentIndex - distance + limit) % limit;
+
+    return (previousIndex == neighborA || previousIndex == neighborB);
   }
 }
